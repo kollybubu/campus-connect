cdc5ecf5211648dbfe5643f40a24986d67c80f7f
kollybubu <kollysama@gmail.com>
2024-10-09 15:18:35 +0630

    user id nk category sit

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -24,7 +24,7 @@ class CourseSeeder extends Seeder
             [
                 'name' => 'Java',
                 'description' => 'Java is a multi-platform, object-oriented, and network-centric language that can be used as a platform in itself. It is a fast, secure, reliable programming language for coding everything from mobile apps and enterprise software to big data applications and server-side technologies.',
-                'image' => 'JAVA.jpg',
+                'image' => 'JAVA.png',
                 'user_id' => 1, // Faculty user
                 'category_id' => 2, // Replace with appropriate category ID
             ],
