560dfeaf26b2f88abaac77e76e2f867563d99cf8
kollybubu <kollysama@gmail.com>
2024-10-13 21:55:24 +0630

    id

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -15,6 +15,7 @@ class CommentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'comment' => $this->comment,
             'created_by' => $this->user->name,
             'post' => $this->post->title
